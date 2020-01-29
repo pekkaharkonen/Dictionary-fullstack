afe87ea9d217ad449d157a0af1e4ab42e01dd4d5
Pekka Härkönen <pekka.o.harkonen@utu.fi>
2020-01-29 14:05:15 +0200

    first api and layout fixed

--- public/app.js
+++ public/app.js
@@ -5,10 +5,13 @@ const results = document.getElementById("results");
 button.addEventListener("click", getWord);
 
 function getWord(e) {
-
   results.innerHTML = "";
   e.preventDefault();
-  fetch(`http://localhost:3000/api/words/${word.value}`)
+
+  let trimmed = (word.value).trim();
+  let input = trimmed.replace(/[\s]+/, "-")
+
+  fetch(`http://localhost:3000/api/words/${input}`)
     .then(response => response.json())
     .then(data => {
       console.log(data);
@@ -21,13 +24,11 @@ function getWord(e) {
 
 function createBox(data) {
   for (let i = 0; i < data.length; i++) {
-
     //Creating a new box with class "box"
     let newBox = document.createElement("div");
     newBox.classList.add("box");
 
     let box = addContent(newBox, data[i]);
-
 
     // newBox.innerHTML = `<p>${JSON.stringify(data)}</p>`
 
@@ -35,57 +36,145 @@ function createBox(data) {
   }
 }
 
-
-function addContent(box, data){
+function addContent(box, data) {
   // Header with the name
   let word = data.word;
-  let header = document.createElement('h2')
+  let header = document.createElement("h2");
   header.innerText = word;
-  box.appendChild(header)
+  box.appendChild(header);
 
   //Phonetic as a p-element
-  let phonetic = document.createElement('p');
-  phonetic.innerText = data.phonetic;
-  box.appendChild(phonetic)
+  let phonetic = document.createElement("p");
+  if(data.phonetic){
+    phonetic.innerHTML = `<i>${data.phonetic}</i>`;
+  }else {
+    phonetic.innerHTML = `<i>I know alot, but I have no idea how to pronounce ${data.word}</i>`
+  }
+  
+  box.appendChild(phonetic);
 
   //meanings:
 
   let meaning = data.meaning;
-  console.log(meaning)
 
+  // Noun?
+  if (meaning["noun"]) {
+    for (let item of meaning["noun"]) {
+      let noun = document.createElement("div");
+      noun.innerHTML = "<b>Noun: </b>";
 
-  if (meaning["noun"]){
-    let noun = document.createElement('div');
-    for(let item of meaning["noun"]){
-      console.log('Noun: ')
-      if(item["definition"]){
-        console.log(item["definition"])
+      if (item["definition"]) {
+        let definition = document.createElement("div");
+        definition.classList.add('secondary-content')
+        definition.innerHTML = "Definition: " + item["definition"];
+        noun.appendChild(definition);
       }
-      if(item["example"]){
-        console.log(item["example"])
+
+      if (item["example"]) {
+        let example = document.createElement("div");
+        example.classList.add('secondary-content')
+        example.innerHTML = "Example: " + item["example"];
+        noun.appendChild(example);
       }
+
+      if (item["synonyms"]) {
+        let synonyms = document.createElement("div");
+        synonyms.classList.add('secondary-content')
+        synonyms.innerHTML = "Synonyms: " + item["synonyms"];
+        noun.appendChild(synonyms);
+      }
+      box.appendChild(noun);
     }
   }
-  if (meaning["transitive verb"]){
-    let text = `Transitive verb:`
-    for(let item of meaning["transitive verb"]){
-      console.log('Transitive verb: ')
-      if(item["definition"]){
-        console.log(item["definition"])
+  // Exclamation?
+  if (meaning["exclamation"]) {
+    for (let item of meaning["exclamation"]) {
+      let exclamation = document.createElement("div");
+      exclamation.innerHTML = "<b>Exclamation: </b>";
+
+      if (item["definition"]) {
+        let definition = document.createElement("div");
+        definition.classList.add('secondary-content')
+        definition.innerHTML = "Definition: " + item["definition"];
+        exclamation.appendChild(definition);
       }
-      if(item["example"]){
-        console.log(item["example"])
+
+      if (item["example"]) {
+        let example = document.createElement("div");
+        example.classList.add('secondary-content')
+        example.innerHTML = "Example: " + item["example"];
+        exclamation.appendChild(example);
       }
+
+      if (item["synonyms"]) {
+        let synonyms = document.createElement("div");
+        synonyms.classList.add('secondary-content')
+        synonyms.innerHTML = "Synonyms: " + item["synonyms"];
+        exclamation.appendChild(synonyms);
+      }
+      box.appendChild(exclamation);
+    }
+  }
+  // Abbreviation?
+  if (meaning["abbreviation"]) {
+    for (let item of meaning["abbreviation"]) {
+      let abbreviation = document.createElement("div");
+      abbreviation.innerHTML = "<b>Abbreviation: </b>";
+
+      if (item["definition"]) {
+        let definition = document.createElement("div");
+        definition.classList.add('secondary-content')
+        definition.innerHTML = "Definition: " + item["definition"];
+        abbreviation.appendChild(definition);
+      }
+
+      if (item["example"]) {
+        let example = document.createElement("div");
+        example.classList.add('secondary-content')
+        example.innerHTML = "Example: " + item["example"];
+        abbreviation.appendChild(example);
+      }
+
+      if (item["synonyms"]) {
+        let synonyms = document.createElement("div");
+        synonyms.classList.add('secondary-content')
+        synonyms.innerHTML = "Synonyms: " + item["synonyms"];
+        abbreviation.appendChild(synonyms);
+      }
+      box.appendChild(abbreviation);
+    }
+  }
+  //Transitive verb?
+  if (meaning["transitive verb"]) {
+    for (let item of meaning["transitive verb"]) {
+      let transitive = document.createElement("div");
+      transitive.innerHTML = "<b>Transitive verb: </b>";
+
+      if (item["definition"]) {
+        let definition = document.createElement("div");
+        definition.classList.add('secondary-content')
+        definition.innerHTML = "Definition: " + item["definition"];
+        transitive.appendChild(definition);
+      }
+
+      if (item["example"]) {
+        let example = document.createElement("div");
+        example.classList.add('secondary-content')
+        example.innerHTML = "Example: " + item["example"];
+        transitive.appendChild(example);
+      }
+
+      if (item["synonyms"]) {
+        let synonyms = document.createElement("div");
+        synonyms.classList.add('secondary-content')
+        synonyms.innerHTML = "Synonyms: " + item["synonyms"];
+        transitive.appendChild(synonyms);
+      }
+      box.appendChild(transitive);
     }
   }
 
-  //transitive verbs as a div - word.meaning.transitive verb
 
 
   return box;
 }
-
-
-// function getKeyByValue(object, value) { 
-//   return Object.keys(object).find(key => object[key] === value)
-// }
